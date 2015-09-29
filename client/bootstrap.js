815ad7fa2097ed05226383f8185b8416b3db20fb
Aren Blondahl <rean1985@gmail.com>
2015-09-29 14:53:43 -0600

    Removed old bootstrap

--- client/bootstrap.js
+++
@@ -1,9 +0,0 @@
-// A quick static server for hosting the compiled project
-// Should be launched with pm2 if possible
-
-var connect         = require('connect'),
-    serveStatic     = require('serve-static');
-
-connect()
-    .use(serveStatic('.'))
-    .listen(8080);

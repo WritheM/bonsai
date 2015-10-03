4e4d798088b8e1192922cab8380bd9b653e5cf1a
Joe Carter <theupperquartile@gmail.com>
2015-10-03 03:31:27 +0100

    Added Session controller

--- server/src/app/app.js
+++ server/src/app/app.js
@@ -8,12 +8,14 @@ models.sequelize.sync();
 
 import Playlists from "./controller/Playlists";
 import PlaylistMedia from "./controller/PlaylistMedia";
+import Session from "./controller/Session";
 
 let router = new Router();
 
 //init controllers here
 router.addController(new Playlists());
 router.addController(new PlaylistMedia());
+router.addController(new Session());
 
 let server = new Rabbit.server(router);
 server.listen();
---
+++ server/src/app/controller/Session.js
@@ -0,0 +1,57 @@
+import {Controller,Route} from "./Controller";
+import models from "../db/models";
+
+export default class Session extends Controller {
+    @Route("Session:register")
+    async register(msg, conn) {
+        let user = await models.User.create({
+            username: msg.username,
+            slug: msg.username,
+            language: msg.username,
+            joined: Date.now(),
+            gRole: 0
+        });
+        return user;
+    }
+
+    @Route("Session:login");
+    async login(msg, conn) {
+        let user = await models.User.findById(msg.user_id);
+        //TODO: password
+
+        let s = await models.Auth.create({
+            user_id: user.id,
+            token: this.makeId(),
+            created: Date.now(),
+            last: Date.()
+        });
+
+        return {user: user, session: s};
+    }
+
+    @Route("Session:loginToken")
+    async loginToken(msg, conn) {
+        let s = await models.Auth.findOne({where: {token: msg.token}});
+        let user = await models.Auth.findById(s.user_id);
+        return {user: user, session: s};
+    }
+
+    @Route("Session:logout")
+    async logout(msg, conn) {
+        let s = await models.Auth.findOne({where: {token: msg.token}});
+        await s.destroy();
+        return true;
+    }
+
+    //http://stackoverflow.com/a/1349426
+    makeid() {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for (var i = 0; i < 5; i++)
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        return text;
+    }
+
+}
